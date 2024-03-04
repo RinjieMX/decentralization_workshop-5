26769d5430bb99cdbbe569985d1de443f28407df
manon gardin <manon.gardin@edu.devinci.fr>
2024-03-04 16:25:00 +0100

    First part : Basic setup

--- src/nodes/node.ts
+++ src/nodes/node.ts
@@ -3,6 +3,13 @@ import express from "express";
 import { BASE_NODE_PORT } from "../config";
 import { Value } from "../types";
 
+export type NodeState = {
+  killed: boolean; // this is used to know if the node was stopped by the /stop route. It's important for the unit tests but not very relevant for the Ben-Or implementation
+  x: 0 | 1 | "?" | null; // the current consensus value
+  decided: boolean | null; // used to know if the node reached finality
+  k: number | null; // current step of the node
+};
+
 export async function node(
   nodeId: number, // the ID of the node
   N: number, // total number of nodes in the network
@@ -16,9 +23,21 @@ export async function node(
   node.use(express.json());
   node.use(bodyParser.json());
 
-  // TODO implement this
-  // this route allows retrieving the current status of the node
-  // node.get("/status", (req, res) => {});
+  let NState: NodeState = {
+    killed: false,
+    x: null,
+    decided: null,
+    k: null
+  };
+
+  node.get("/status", (req, res) => {
+    if (isFaulty){
+      res.status(500).send('faulty');
+    }
+    else {
+      res.status(200).send('live');
+    }
+  });
 
   // TODO implement this
   // this route allows the node to receive messages from other nodes
@@ -34,7 +53,11 @@ export async function node(
 
   // TODO implement this
   // get the current state of a node
-  // node.get("/getState", (req, res) => {});
+  node.get("/getState", (req, res) => {
+    if (NState){
+      res.status(200).json(NState);
+    }
+  });
 
   // start the server
   const server = node.listen(BASE_NODE_PORT + nodeId, async () => {
