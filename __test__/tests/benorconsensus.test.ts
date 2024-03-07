6fbf641348b692cdfc487b4269307c6f10de2403
manon gardin <manon.gardin@edu.devinci.fr>
2024-03-07 13:52:54 +0100

    Augmented timelimit for test

--- __test__/tests/benorconsensus.test.ts
+++ __test__/tests/benorconsensus.test.ts
@@ -127,7 +127,7 @@ describe("Ben-Or decentralized consensus algorithm", () => {
       servers.splice(0);
     });
 
-    const timeLimit = 2000; // 2s
+    const timeLimit = 5000; // Augmented to 5s (else, the Exceeding Fault tolerance test sometimes doesn't pass)
 
     it("Finality is reached - Unanimous Agreement - 2 pt", async () => {
       const faultyArray = [false, false, false, false, false];
